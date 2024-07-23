ee4867f0e5a47976f556e3752100e3b595f9be60
Timothée Dione <timothee.dione@gmail.com>
2024-07-23 22:26:22 +0200

    savana background: basic images are set up

--- savana-animation/savana-animated-background.mjs
+++ savana-animation/savana-animated-background.mjs
@@ -15,8 +15,12 @@ function reRender(time) {
   ctx.canvas.width = ctx.canvas.clientWidth;
   mountains.draw(ctx);
   ground.draw(ctx);
+  grass.draw(ctx);
   trunk.draw(ctx);
+  treeBottomStrokeHider.draw(ctx);
   leafs.draw(ctx);
+  clouds.draw(ctx);
+  birds.draw(ctx);
   requestAnimationFrame(reRender);
 }
 requestAnimationFrame(reRender);
@@ -29,12 +33,11 @@ const animationReferenceFrame = {
   xOffset: ctx.canvas.width - ctx.canvas.clientHeight / 0.65,
 };
 const desktopLandscapeElementsGeometry = {
-  treeBottomStrokeHider: {},
-  grass: {
-    width: animationReferenceFrame.width * 0.72,
-    height: animationReferenceFrame.height * 0.1,
-    x: animationReferenceFrame.width * 0.09 + animationReferenceFrame.xOffset,
-    y: animationReferenceFrame.height * 0.89,
+  treeBottomStrokeHider: {
+    width: animationReferenceFrame.width * 0.12,
+    height: animationReferenceFrame.height * 0.05,
+    x: animationReferenceFrame.width * 0.67 + animationReferenceFrame.xOffset,
+    y: animationReferenceFrame.height * 0.93,
   },
   ground: {
     width: animationReferenceFrame.width * 1.02,
@@ -44,6 +47,12 @@ const desktopLandscapeElementsGeometry = {
       animationReferenceFrame.xOffset,
     y: animationReferenceFrame.height * 0.87,
   },
+  grass: {
+    width: animationReferenceFrame.width * 0.72,
+    height: animationReferenceFrame.height * 0.1,
+    x: animationReferenceFrame.width * 0.09 + animationReferenceFrame.xOffset,
+    y: animationReferenceFrame.height * 0.93,
+  },
   mountains: {
     width: animationReferenceFrame.width * 0.67,
     height: animationReferenceFrame.height * 0.25,
@@ -52,8 +61,18 @@ const desktopLandscapeElementsGeometry = {
       animationReferenceFrame.xOffset,
     y: animationReferenceFrame.height * 0.77,
   },
-  birds: {},
-  clouds: {},
+  birds: {
+    width: animationReferenceFrame.width * 0.06,
+    height: animationReferenceFrame.height * 0.07,
+    x: animationReferenceFrame.width * 0.86 + animationReferenceFrame.xOffset,
+    y: animationReferenceFrame.height * 0.24,
+  },
+  clouds: {
+    width: animationReferenceFrame.width * 0.56,
+    height: animationReferenceFrame.height * 0.09,
+    x: animationReferenceFrame.width * 0.35 + animationReferenceFrame.xOffset,
+    y: animationReferenceFrame.height * 0.15,
+  },
   leafs: {
     width: animationReferenceFrame.width * 0.35,
     height: animationReferenceFrame.height * 0.26,
@@ -101,10 +120,34 @@ const ground = new ImageToDraw({
   baseHeight: desktopLandscapeElementsGeometry.ground.height,
 });
 
+const grass = new ImageToDraw({
+  imagePath: './assets/images/floor/grass.svg',
+  x: desktopLandscapeElementsGeometry.grass.x,
+  y: desktopLandscapeElementsGeometry.grass.y,
+  baseWidth: desktopLandscapeElementsGeometry.grass.width,
+  baseHeight: desktopLandscapeElementsGeometry.grass.height,
+});
+
 const treeBottomStrokeHider = new ImageToDraw({
   imagePath: './assets/images/tree/bottom-stroke-hide.svg',
-  x: desktopLandscapeElementsGeometry.ground.x,
-  y: desktopLandscapeElementsGeometry.ground.y,
-  baseWidth: desktopLandscapeElementsGeometry.ground.width,
-  baseHeight: desktopLandscapeElementsGeometry.ground.height,
+  x: desktopLandscapeElementsGeometry.treeBottomStrokeHider.x,
+  y: desktopLandscapeElementsGeometry.treeBottomStrokeHider.y,
+  baseWidth: desktopLandscapeElementsGeometry.treeBottomStrokeHider.width,
+  baseHeight: desktopLandscapeElementsGeometry.treeBottomStrokeHider.height,
+});
+
+const clouds = new ImageToDraw({
+  imagePath: './assets/images/sky/clouds.svg',
+  x: desktopLandscapeElementsGeometry.clouds.x,
+  y: desktopLandscapeElementsGeometry.clouds.y,
+  baseWidth: desktopLandscapeElementsGeometry.clouds.width,
+  baseHeight: desktopLandscapeElementsGeometry.clouds.height,
+});
+
+const birds = new ImageToDraw({
+  imagePath: './assets/images/sky/birds.svg',
+  x: desktopLandscapeElementsGeometry.birds.x,
+  y: desktopLandscapeElementsGeometry.birds.y,
+  baseWidth: desktopLandscapeElementsGeometry.birds.width,
+  baseHeight: desktopLandscapeElementsGeometry.birds.height,
 });
